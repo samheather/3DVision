0323fb9527fa8de55c9225330d160a565a4f65fa
Sam Heather <samh1@heather.sh>
2013-01-23 15:41:45 +0000

    Threading 1

--- second-program.py
+++ second-program.py
@@ -1,42 +1,13 @@
 import cv2
 import cv2.cv as cv
 import threading
+
 print "Press Escape to Quit"
 
-defaultCamera = cv2.VideoCapture(0) #set defaultCamera as a video stream from system camera 0
-s, img = defaultCamera.read()
-cascadeFile = cv2.CascadeClassifier('C:\opencv\data\haarcascades\haarcascade_frontalface_default.xml')
-lastFace = [0,0,0,0]
+class MyThread(threading.thread):
+    def threadOne(self):
+        print 'running first'
 
-def facedetect(img, lastFaceFound, faceCascade):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    gray = cv2.equalizeHist(gray)
-    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2, minSize=(50, 50), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
-    if len(faces) > 0:
-        lastFaceFound = faces[0]
-    draw_3d(lastFaceFound)
-    return lastFaceFound
-
-def draw_3d(point):
-    print point
-    cv2.circle(img, (point[0]+point[2]/2,point[1]+point[2]/2), point[2]/2, 255, -1)
-
-streamWinName = "Camera Stream"
-threeDWinName = "3D View"
-cv2.namedWindow(streamWinName, cv2.CV_WINDOW_AUTOSIZE)
-cv2.namedWindow(threeDWinName, cv2.CV_WINDOW_AUTOSIZE)
-##cv2.resizeWindow(threeDWinName, 600, 600)
-img2 = cv.CreateImage((400,400), 8, 3)
-
-while s:
-##    Put img's into windows and update img
-    cv2.imshow(streamWinName,img)
-    cv2.imshow(threeDWinName,img)
-    s, img = defaultCamera.read()
-##    Call facedetect and draw 
-    lastFace = facedetect(img, lastFace, cascadeFile)
-##    Exit method
-    escapeKey = cv2.waitKey(10)
-    if escapeKey == 27:
-        cv2.destroyAllWindows()
-        break
+class threadTwo(threading.thread):
+    def run(self):
+        print 'running second'
