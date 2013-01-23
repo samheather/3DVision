65e0153e1a2f7924ee33426529a9e191f8a3bf19
Sam Heather <samh1@heather.sh>
2013-01-23 15:38:28 +0000

    Before Threads

--- second-program.py
+++ second-program.py
@@ -1,14 +1,14 @@
 import cv2
 import cv2.cv as cv
-## ha ha ha
+import threading
 print "Press Escape to Quit"
 
 defaultCamera = cv2.VideoCapture(0) #set defaultCamera as a video stream from system camera 0
 s, img = defaultCamera.read()
+cascadeFile = cv2.CascadeClassifier('C:\opencv\data\haarcascades\haarcascade_frontalface_default.xml')
 lastFace = [0,0,0,0]
 
-def facedetect(img, lastFaceFound):
-    faceCascade = cv2.CascadeClassifier('C:\opencv\data\haarcascades\haarcascade_frontalface_default.xml')
+def facedetect(img, lastFaceFound, faceCascade):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     gray = cv2.equalizeHist(gray)
     faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2, minSize=(50, 50), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
@@ -34,7 +34,7 @@ while s:
     cv2.imshow(threeDWinName,img)
     s, img = defaultCamera.read()
 ##    Call facedetect and draw 
-    lastFace = facedetect(img, lastFace)
+    lastFace = facedetect(img, lastFace, cascadeFile)
 ##    Exit method
     escapeKey = cv2.waitKey(10)
     if escapeKey == 27:
