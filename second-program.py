417bcde4f952b2c9895b2a8f1220e7ccb5207010
Sam Heather <samh1@heather.sh>
2013-01-27 14:50:56 +0000

    Getting images sent across

--- second-program.py
+++ second-program.py
@@ -5,7 +5,7 @@ import threading
 
 print "Press Escape to Quit"
 
-lastPosition = None
+lastPosition = [10,10,10]
 
 class threadOne(threading.Thread):
     def run(self):
@@ -16,10 +16,12 @@ class threadOne(threading.Thread):
         defaultCamera = cv2.VideoCapture(0) #set defaultCamera as a video stream from system camera 0
         s, img = defaultCamera.read()
         cascadeFile = cv2.CascadeClassifier('C:\opencv\data\haarcascades\haarcascade_frontalface_default.xml')
-        lastFace = [0,0,0,0]
+        lastFace = [200,40,240,240]
         
         streamWinName = "Camera Stream"
         cv2.namedWindow(streamWinName, cv2.CV_WINDOW_AUTOSIZE)
+        videoWidth = defaultCamera.get(cv.CV_CAP_PROP_FRAME_WIDTH)
+        videoHeight = defaultCamera.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
 
         while s:
 ##          Put img's into windows and update img
@@ -27,21 +29,22 @@ class threadOne(threading.Thread):
             s, img = defaultCamera.read()
 ##           Call facedetect and draw 
             lastFace = self.facedetect(img, lastFace, cascadeFile)
-            viewerPosition = self.estimateViewerPosition(lastFace)
-            with lock:
-                lastPosition = viewerPosition
+##            viewerPosition = self.estimateViewerPosition(lastFace, videoHeight, videoWidth)
+##            with lock:
+##                lastPosition = viewerPosition
 ##          Exit method
             escapeKey = cv2.waitKey(10)
             if escapeKey == 27:
-                print 'Goodbye'
-                cv2.destroyAllWindows()
+                print 'Goodbye 1'
+                cv2.destroyWindow('Camera Stream')
                 break
 
     def facedetect(self, img, lastFaceFound, faceCascade):
         gray = self.convertToGray(img)
-        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2, minSize=(50, 50), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2, minSize=(100, 100), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
         if len(faces) > 0:
             lastFaceFound = faces[0]
+        print lastFaceFound
         return lastFaceFound
 
     def convertToGray(self, inputImg):
@@ -49,17 +52,18 @@ class threadOne(threading.Thread):
         converted = cv2.equalizeHist(converted)
         return converted
 
-    def estimateViewerPosition(self, face):
-        windowWidth = 640
-        windowHeight = 480
+    def estimateViewerPosition(self, face, videoWidth, videoHeight):
         frameFromSide = face[0]
         frameFromTop = face[1]
         frameSize = face[2]
 ##        Z is distance from camera, Y is elevation and X is X position.
-        Z = 10*frameSize
-        X = frameFromSide
-        frameCenterYFromTop = frameFromTop + frameSize/2
-        Y = 0
+        C = 0.1
+        Z = int(30/(frameSize*C))
+        X = int(frameFromSide + frameSize/2)
+##        frameCenterYFromTop = frameFromTop + frameSize/2
+##        Y = int(-(frameCenterYFromTop - videoHeight/2))
+        Y = frameFromTop + (frameSize/2) - (videoHeight/2)
+        print Y
         return [X,Y,Z]
 
 
@@ -69,20 +73,29 @@ class threadTwo(threading.Thread):
         
         threeDWinName = "3D View"
         cv2.namedWindow(threeDWinName, cv2.CV_WINDOW_AUTOSIZE)
-        img2 = np.zeros((600,600),np.float32)
+        img2 = cv2.imread('white.png', 0)
+        cv2.imshow(threeDWinName,img2)
+        cv2.circle(img2, (100,100),100,255,1)
         cv2.imshow(threeDWinName,img2)
         
         while True:
             with lock:
                 viewerPosition = lastPosition
-            img2 = self.render(viewerPosition, img2)
+##            img2 = self.render(viewerPosition, img2)
 
     def render(self, positionToRenderFor, targetImg):
-        cv2.circle(targetImg, (positionToRenderFor[0], positionToRenderFor[1]), positionToRenderFor[2]/2, 255, -1)
+        print positionToRenderFor
+##      Exit method
+        escapeKey = cv2.waitKey(10)
+        if escapeKey == 27:
+            print 'Goodbye 2'
+            cv2.destroyWindow('3D View')
+##        cv2.circle(targetImg, (positionToRenderFor[0], positionToRenderFor[1]), positionToRenderFor[2]/2, 255, -1)
+        cv2.circle(targetImg, (100,100),100,255,-1)
         return targetImg
         
         
 lock = threading.Lock()
 
-threadOne().start()
+##threadOne().start()
 threadTwo().start()
