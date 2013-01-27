ea384f52f73b2aeade11cfafcab1f889c085f520
Sam Heather <samh1@heather.sh>
2013-01-27 15:55:46 +0000

    Uncalibrated, makes blue bots, before openGL

--- second-program.py
+++ second-program.py
@@ -29,9 +29,9 @@ class threadOne(threading.Thread):
             s, img = defaultCamera.read()
 ##           Call facedetect and draw 
             lastFace = self.facedetect(img, lastFace, cascadeFile)
-##            viewerPosition = self.estimateViewerPosition(lastFace, videoHeight, videoWidth)
-##            with lock:
-##                lastPosition = viewerPosition
+            viewerPosition = self.estimateViewerPosition(lastFace, videoHeight, videoWidth)
+            with lock:
+                lastPosition = viewerPosition
 ##          Exit method
             escapeKey = cv2.waitKey(10)
             if escapeKey == 27:
@@ -44,7 +44,6 @@ class threadOne(threading.Thread):
         faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2, minSize=(100, 100), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
         if len(faces) > 0:
             lastFaceFound = faces[0]
-        print lastFaceFound
         return lastFaceFound
 
     def convertToGray(self, inputImg):
@@ -57,13 +56,12 @@ class threadOne(threading.Thread):
         frameFromTop = face[1]
         frameSize = face[2]
 ##        Z is distance from camera, Y is elevation and X is X position.
-        C = 0.1
+        C = 0.01
         Z = int(30/(frameSize*C))
         X = int(frameFromSide + frameSize/2)
 ##        frameCenterYFromTop = frameFromTop + frameSize/2
 ##        Y = int(-(frameCenterYFromTop - videoHeight/2))
-        Y = frameFromTop + (frameSize/2) - (videoHeight/2)
-        print Y
+        Y = int(frameFromTop + (frameSize/2) - (videoHeight/2))
         return [X,Y,Z]
 
 
@@ -73,29 +71,33 @@ class threadTwo(threading.Thread):
         
         threeDWinName = "3D View"
         cv2.namedWindow(threeDWinName, cv2.CV_WINDOW_AUTOSIZE)
-        img2 = cv2.imread('white.png', 0)
+        img2 = cv2.imread('white.png', -1)
         cv2.imshow(threeDWinName,img2)
-        cv2.circle(img2, (100,100),100,255,1)
+##        cv2.circle(img2, (100,100),100,255,1)
         cv2.imshow(threeDWinName,img2)
         
         while True:
             with lock:
                 viewerPosition = lastPosition
-##            img2 = self.render(viewerPosition, img2)
+            img2 = self.render(viewerPosition, img2)
+            if (img2 == None):
+                break
 
     def render(self, positionToRenderFor, targetImg):
-        print positionToRenderFor
+        threeDWinName = "3D View"
+        print 'render', positionToRenderFor
 ##      Exit method
         escapeKey = cv2.waitKey(10)
         if escapeKey == 27:
             print 'Goodbye 2'
-            cv2.destroyWindow('3D View')
-##        cv2.circle(targetImg, (positionToRenderFor[0], positionToRenderFor[1]), positionToRenderFor[2]/2, 255, -1)
-        cv2.circle(targetImg, (100,100),100,255,-1)
+            cv2.destroyWindow(threeDWinName)
+            return None
+        cv2.circle(targetImg, (positionToRenderFor[0], positionToRenderFor[1]+180), positionToRenderFor[2]/2, 255, -1)
+        cv2.imshow(threeDWinName,targetImg)
         return targetImg
         
         
 lock = threading.Lock()
 
-##threadOne().start()
+threadOne().start()
 threadTwo().start()
