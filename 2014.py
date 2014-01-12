41f65b39e2c740032e3c13063e9b0dc8686fc82d
samheather <sam@heather.sh>
2014-01-11 16:35:39 -0800

    further progress with vision, just starting on rendering cube again

--- 2014.py
+++ 2014.py
@@ -1,12 +1,17 @@
 import cv2
+import cv2.cv as cv
+from random import randrange
 
-def setup():
+count = 0
+
+def simple():
+	global count
 	defaultCamera = cv2.VideoCapture(0)
 	s, img = defaultCamera.read()
 	cascadeFile = cv2.CascadeClassifier('OpenCV/opencv-2.4.8/data/haarcascades/haarcascade_frontalface_default.xml')
 	
 	streamWinName = "Camera Stream"
-	cv2.namedWindow(streamWinName, cv2.CV_WINDOW_AUTOSIZE)
+	cv2.namedWindow(streamWinName, cv.CV_WINDOW_NORMAL)
 
 	while s:
 		cv2.imshow(streamWinName,img)
@@ -14,7 +19,14 @@ def setup():
 		gray = convertToGray(img)
 		faces = cascadeFile.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2, minSize=(10, 10), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
         	if len(faces) > 0:
-	 		print "Face Found"
+			# [top left x, top left y, width, height]
+			centreX = faces[0][0]+faces[0][2]/2
+			centreY = faces[0][1]+faces[0][3]/2			
+			diameter = (faces[0][2]+faces[0][3])/3.5
+	 		count += 1
+			if (count%70 == 15):			
+				print faces[0]
+			cv2.circle(img, (centreX, centreY), diameter, (100,100,255))
 
             	#Exit method
             	escapeKey = cv2.waitKey(10)
@@ -28,4 +40,4 @@ def convertToGray(inputImg):
         return cv2.equalizeHist(converted)
         
 
-setup()
+simple()
